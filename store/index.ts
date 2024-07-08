159b2270346ed2fad745402a4cb5283c35d484f9
Gareth le Grange <garethlegrange@gmail.com>
2024-07-08 09:55:43 +0200

    Update index.ts

--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import type { Annotation } from "@/types";
 
+// Define the store
 interface Store {
   annotations: Annotation[];
   setAnnotations: (annotations: Annotation[]) => void;
@@ -11,7 +12,10 @@ interface Store {
 }
 
 export const useStore = create<Store>((set) => ({
+  // Initial state
   annotations: [],
+  // Actions
+  // Set the annotations and set them all to visible
   setAnnotations: (annotations) =>
     set({
       annotations: annotations.map((annotation) => ({
@@ -19,30 +23,41 @@ export const useStore = create<Store>((set) => ({
         visible: true,
       })),
     }),
+  // Show all annotations
   showAnnotations: true,
+  // Toggle all annotations
   toggleAllAnnotations: () =>
     set((state) => {
+        // Toggle the showAnnotations state
         const showAnnotations = !state.showAnnotations;
+
+        // Map over the annotations and set them to visible or not based on the showAnnotations state
         const annotations = state.annotations.map((annotation) => ({
             ...annotation,
             visible: showAnnotations,
         }));
     
+        // Return the new state
         return { showAnnotations, annotations };
     }),
+  // Toggle an annotation by class_uuid
   toggleAnnotation: (class_uuid) => {
     set((state) => {
+      // Map over the annotations and toggle the visibility of the annotation with the matching class_uuid
       const annotations = state.annotations.map((annotation) =>
         annotation.class_uuid === class_uuid
           ? { ...annotation, visible: !annotation.visible }
           : annotation
       );
 
+      // Return the new state
       return { annotations };
     });
   },
+  // Search annotations by class_name
   searchAnnotations: (search) => {
     set((state) => {
+      // Map over the annotations and set the visibility based on the search term
       const annotations = state.annotations.map((annotation) => ({
         ...annotation,
         visible: annotation.class_name
@@ -50,6 +65,7 @@ export const useStore = create<Store>((set) => ({
           .includes(search.toLowerCase()),
       }));
 
+      // Return the new state
       return { annotations };
     });
   },
